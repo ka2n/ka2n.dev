11bdd6104a9001632b02cc2b9c02ca11747320ec
Katsuma Ito <katsumai@gmail.com>
2021-06-23 01:34:02 +0900

    chore: use next/script to load GTM

--- apps/blog/components/GoogleAnalytics.tsx
+++ apps/blog/components/GoogleAnalytics.tsx
@@ -1,4 +1,4 @@
-import Head from "next/head";
+import Script from "next/script";
 
 export const GoogleAnalytics = ({ gtag }: { gtag: string }) => {
   return (
@@ -10,25 +10,11 @@ export const GoogleAnalytics = ({ gtag }: { gtag: string }) => {
 };
 
 const Analytics = ({ gtag }: { gtag: string }) => (
-  <>
-    <Head>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  <Script strategy="afterInteractive">{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${gtag}');`,
-        }}
-      />
-    </Head>
-    <noscript
-      dangerouslySetInnerHTML={{
-        __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${gtag}"
-height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-      }}
-    />
-  </>
+})(window,document,'script','dataLayer','${gtag}');`}</Script>
 );
 
 const GoogleAnalyticsNoAMPTracker: React.FC<{ gtag: string }> = (props) => {
