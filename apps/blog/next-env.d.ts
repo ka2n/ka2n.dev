b427722c4e7866387c70de37a3935d53109f44ed
Katsuma Ito <katsumai@gmail.com>
2021-06-24 01:23:28 +0900

    fix: typing added by next

--- apps/blog/next-env.d.ts
+++ apps/blog/next-env.d.ts
@@ -1,10 +1,10 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+/// <reference types="next/image-types/global" />
 
 declare module "konva-node" {
   export * from "konva";
   import Konva from "konva";
   import canvas from "canvas";
-
   export = Konva;
 }
