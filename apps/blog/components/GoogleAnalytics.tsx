434a069ff2f5efa2adcf6ebff7ac04204ed75472
Katsuma Ito <katsumai@gmail.com>
2021-11-16 10:34:04 +0900

    fix(lint): add id to Script tag

--- apps/blog/components/GoogleAnalytics.tsx
+++ apps/blog/components/GoogleAnalytics.tsx
@@ -11,7 +11,10 @@ export const GoogleAnalytics = ({ gtag }: { gtag: string }) => {
 };
 
 const Analytics = ({ gtag }: { gtag: string }) => (
-  <Script strategy="afterInteractive">{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  <Script
+    id="ga"
+    strategy="afterInteractive"
+  >{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
